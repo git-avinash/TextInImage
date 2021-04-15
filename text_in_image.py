7e094acfe644bac66274ea577ea0e1dbbf442a32
git-avinash <mail.avinashsah@gmail.com>
2021-04-15 11:12:41 +0530

    more precise now

--- text_in_image.py
+++ text_in_image.py
@@ -1,16 +1,23 @@
 from PIL import Image
 import pytesseract
 import sys
+import cv2
 
 # Default Installation path when installed for everyone
 # pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 # Custom Path
-pytesseract.pytesseract.tesseract_cmd = r".\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"./Tesseract-OCR/tesseract.exe"
 
+def make_grey_scale(image_name):
+    img = cv2.imread(image_name)
+    grey_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("grey_scaled.png", grey_image)
 
 def get_text(image_name):
-    with Image.open(image_name) as image:
+    make_grey_scale(image_name)
+
+    with Image.open("grey_scaled.png") as image:
         text = pytesseract.image_to_string(image, lang="eng")
 
     return text
